34f08062af83e23ae36eec652c8da8607888887e
Lucas.Rodrigues <Lucas.Rodrigues@ab-inbev.com>
2024-04-23 19:19:01 -0300

    Refactor Button component and add hover effect in Button.jsx and Button.module.css

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,14 +1,9 @@
-
 import styles from "./Button.module.css";
 import PropTypes from "prop-types";
 export function Button({ children, onClick }) {
   return (
     <div className={styles.container}>
-      <button onClick={onClick} className={styles.button}>
-        <svg width="180px" height="60px" viewBox="0 0 180 60" className={styles.svgBorder}>
-          <polyline points="179,1 179,59 1,59 1,1 179,1" className={styles.bgLine} />
-          <polyline points="179,1 179,59 1,59 1,1 179,1" className={styles.hlLine} />
-        </svg>
+      <button onClick={onClick} className={styles["btn-6"]}>
         <span>{children}</span>
       </button>
     </div>
