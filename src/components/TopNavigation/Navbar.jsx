e53701756c8ba1d6706f64b1e054bafa2e58b17e
Lucas.Rodrigues <Lucas.Rodrigues@ab-inbev.com>
2024-04-25 01:09:29 -0300

    Add smooth scrolling behavior to navigation links

--- src/components/TopNavigation/Navbar.jsx
+++ src/components/TopNavigation/Navbar.jsx
@@ -1,15 +1,17 @@
-import { Link } from 'react-router-dom';
+// Importe Link de react-scroll
+import { Link } from 'react-scroll';
 import styles from './Navbar.module.css';
 
 export function Navbar() {
   return (
     <nav className={styles.navbar}>
       <ul className={styles.navbarList}>
-        <li><Link to="/about">About me</Link></li>
-        <li><Link to="/experiences">Experiences</Link></li>
-        <li><Link to="/skills">Skills</Link></li>
-        <li><Link to="/projects">Projects</Link></li>
-        <li><Link to="/contact">Contact me</Link></li>
+        {/* Adicione 'spy' e 'hashSpy' para atualizar a URL ao rolar */}
+        <li><Link to="about" spy={true} hashSpy={true} smooth={true} duration={500}>About me</Link></li>
+        <li><Link to="experiences" spy={true} hashSpy={true} smooth={true} duration={500}>Experiences</Link></li>
+        <li><Link to="skills" spy={true} hashSpy={true} smooth={true} duration={500}>Skills</Link></li>
+        <li><Link to="projects" spy={true} hashSpy={true} smooth={true} duration={500}>Projects</Link></li>
+        <li><Link to="contact" spy={true} hashSpy={true} smooth={true} duration={500}>Contact me</Link></li>
       </ul>
     </nav>
   );
