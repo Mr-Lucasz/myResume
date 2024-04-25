fc503678d0342c81a350788ec1a4ae4040480544
Lucas.Rodrigues <Lucas.Rodrigues@ab-inbev.com>
2024-04-25 10:24:24 -0300

    Refactor CSS styles in Global.module.css, Header.module.css, and Main.module.css

--- src/components/TopNavigation/Navbar.jsx
+++ src/components/TopNavigation/Navbar.jsx
@@ -3,15 +3,17 @@ import { Link } from 'react-scroll';
 import styles from './Navbar.module.css';
 
 export function Navbar() {
+
+  const headerOffset = -96;
+
   return (
     <nav className={styles.navbar}>
       <ul className={styles.navbarList}>
-        {/* Adicione 'spy' e 'hashSpy' para atualizar a URL ao rolar */}
-        <li><Link to="about" spy={true} hashSpy={true} smooth={true} duration={500}>About me</Link></li>
-        <li><Link to="experiences" spy={true} hashSpy={true} smooth={true} duration={500}>Experiences</Link></li>
-        <li><Link to="skills" spy={true} hashSpy={true} smooth={true} duration={500}>Skills</Link></li>
-        <li><Link to="projects" spy={true} hashSpy={true} smooth={true} duration={500}>Projects</Link></li>
-        <li><Link to="contact" spy={true} hashSpy={true} smooth={true} duration={500}>Contact me</Link></li>
+        <li><Link to="about" spy={true} hashSpy={true} smooth={true} duration={500} offset={headerOffset}>About me</Link></li>
+        <li><Link to="experiences" spy={true} hashSpy={true} smooth={true} duration={500} offset={headerOffset}>Experiences</Link></li>
+        <li><Link to="skills" spy={true} hashSpy={true} smooth={true} duration={500} offset={headerOffset}>Skills</Link></li>
+        <li><Link to="projects" spy={true} hashSpy={true} smooth={true} duration={500} offset={headerOffset}>Projects</Link></li>
+        <li><Link to="contact" spy={true} hashSpy={true} smooth={true} duration={500} offset={headerOffset}>Contact me</Link></li>
       </ul>
     </nav>
   );
