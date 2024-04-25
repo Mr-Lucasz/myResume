18298b52e0095c329995ce16571219a9e1ddc74b
Lucas.Rodrigues <Lucas.Rodrigues@ab-inbev.com>
2024-04-25 01:44:39 -0300

    Refactor CSS styles in App.module.css, Header.module.css, and Main.module.css

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,15 +1,14 @@
-import { useState } from 'react';
-import { Background } from '../components/Background';
-import { Header } from '../components/TopNavigation/Header';
-import { Navbar } from '../components/TopNavigation/Navbar'; // Não se esqueça de importar o Navbar
-import { LoadingScreen } from '../components/LoadingScreen';
-import { AboutMe } from './sections/AboutMe';
-import { Experiences } from './sections/Experiences';
-import { Skills } from './sections/Skills';
-import { Projects } from './sections/Projects';
-import { Contact } from './sections/Contact';
-import styles from './Main.module.css';
-import AnimatedCursor from 'react-animated-cursor';
+import { useState } from "react";
+import { Background } from "../components/Background";
+import { Header } from "../components/TopNavigation/Header";
+import { LoadingScreen } from "../components/LoadingScreen";
+import { AboutMe } from "./sections/AboutMe";
+import { Experiences } from "./sections/Experiences";
+import { Skills } from "./sections/Skills";
+import { Projects } from "./sections/Projects";
+import { Contact } from "./sections/Contact";
+import styles from "./Main.module.css";
+import AnimatedCursor from "react-animated-cursor";
 
 export function Main() {
   const [isLoading, setIsLoading] = useState(false);
@@ -31,16 +30,18 @@ export function Main() {
   if (showContent) {
     return (
       <div className={styles.page}>
-        <AnimatedCursor color="193, 11, 111" />
-        <Header />
-        <main className={styles.mainContent}>
-          {/* Renderize todas as seções diretamente aqui */}
-          <AboutMe />
-          <Experiences />
-          <Skills />
-          <Projects />
-          <Contact />
-        </main>
+        <Background>
+          <AnimatedCursor color="193, 11, 111" />
+          <Header />
+          <main className={styles.mainContent}>
+            {/* Renderize todas as seções diretamente aqui */}
+            <AboutMe />
+            <Experiences />
+            <Skills />
+            <Projects />
+            <Contact />
+          </main>
+        </Background>
       </div>
     );
   }
