340ee3f5b460a008f9c5d491df4a803ed9bc8233
Lucas.Rodrigues <Lucas.Rodrigues@ab-inbev.com>
2024-04-25 17:04:48 -0300

    Add GSAP library for animations and update dependencies

--- src/Global.jsx
+++ src/Global.jsx
@@ -1,29 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { App } from './App.jsx';
-import { BrowserRouter } from 'react-router-dom';
-import './Global.module.css';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { App } from "./App.jsx";
+import { BrowserRouter } from "react-router-dom";
+import "./Global.module.css";
 
-const sections = document.querySelectorAll('section');
-
-const observer = new IntersectionObserver((entries) => {
-  entries.forEach((entry) => {
-    if (entry.isIntersecting) {
-      entry.target.classList.add('fadeIn');
-    } else {
-      entry.target.classList.remove('fadeIn');
-    }
-  });
-});
-
-sections.forEach((section) => {
-  observer.observe(section);
-});
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-   <BrowserRouter>
+    <BrowserRouter>
       <App />
     </BrowserRouter>
   </React.StrictMode>
--- src/pages/sections/AboutMe.jsx
+++ src/pages/sections/AboutMe.jsx
@@ -2,7 +2,7 @@ import styles from "./AboutMe.module.css";
 
 export function AboutMe() {
   return (
-    <section id="about" className={styles.sectionAbout}>
+    <section id="about" className={styles.sectionAbout} >
       <h2>About me</h2>
       <p className={styles.paragraph}>
         Im a front-end developer with a passion for creating beautiful and
